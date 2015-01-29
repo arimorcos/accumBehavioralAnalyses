20b47b2d15448f452657c82649739de1dd1d3a11
Ari Morcos <arimorcos@gmail.com>
2015-01-29 09:47:30 -0500

    getNetEvidence can take mazePatterns
    Updated getNetEvidence to take mazePatterns as well

--- Random Segment Noise/getNetEvidence.m
+++ Random Segment Noise/getNetEvidence.m
@@ -2,7 +2,7 @@ function netEv = getNetEvidence(dataCell)
 %getNetEvidence.m Extracts net evidence readings
 %
 %INPUTS
-%dataCell - dataCell containing integration information
+%dataCell - dataCell containing integration information || mazePatterns
 %
 %OUTPUTS
 %netEv - nTrials x nSeg array containing net evidence. Positive means
@@ -11,7 +11,11 @@ function netEv = getNetEvidence(dataCell)
 %ASM 7/14
 
 %get mazePatterns
-mazePatterns = getMazePatterns(dataCell);
+if iscell(dataCell)
+    mazePatterns = getMazePatterns(dataCell);
+else
+    mazePatterns = dataCell;
+end
 
 %set 0 values to - 1
 mazePatterns(mazePatterns == 0) = -1;
