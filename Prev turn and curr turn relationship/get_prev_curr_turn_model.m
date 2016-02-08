15b33b7d877eaf7474738efd61ff0e55573d8879
Ari Morcos <arimorcos@gmail.com>
2016-02-08 17:32:48 -0500

    Added better prev turn relationship model

---
+++ Prev turn and curr turn relationship/get_prev_curr_turn_model.m
@@ -0,0 +1,12 @@
+function model = get_prev_curr_turn_model(dataCell)
+% calculates a linear model based on the previous reward and turn.
+
+prev_turn = getCellVals(dataCell, 'result.prevTurn');
+prev_reward = getCellVals(dataCell, 'result.prevCorrect');
+curr_turn = getCellVals(dataCell, 'result.leftTurn');
+
+% create table 
+tbl = table(prev_turn', prev_reward', curr_turn');
+
+% create model 
+model = fitlm(tbl);
